c37d60f78510e6981b084741879c91be474a9964
mamecom <mamesan.jp@gmail.com>
2022-12-12 19:30:50 +0900

    商品情報の構造体実装

--- main.go
+++ main.go
@@ -5,11 +5,21 @@ import (
 
 )
 
+type Product_Info struct {
+	name string			//商品名
+	cost_price int 		//原価
+	selling_price int	//売価
+	list_price int		//定価
+	stock int			//在庫
+	product_code string //商品コード
+}
+
 func main() {
 
 	menu()
 }
 
+//　メニュー画面 
 func menu() {
 	println("===========商品管理システム===========")
 	println(" [追加: 1, 削除: 2, 更新: 3, 終了: 0]\n")
@@ -21,7 +31,7 @@ func menu() {
 
 		switch str {
 		case 1:
-			println("a")
+			add_product()
 		case 2:
 			println("b")
 		case 3:
@@ -33,3 +43,4 @@ func menu() {
 		}
 	}
 }
+
