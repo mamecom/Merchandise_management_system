ee4dad49f55b38d856efe3e5229db20fc3baed28
mamecom <mamesan.jp@gmail.com>
2022-12-21 20:31:36 +0900

    商品追加機能実装

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-	"io/ioutil"
-	"io"
+	// "io/ioutil"
 	"log"
 	"encoding/csv"
 	"strconv"
@@ -19,6 +18,10 @@ const (
 
 func main() {
 
+	err := FileInit()
+	if err != nil {
+		os.Exit(1)
+	}
 	result := Menu()
 	os.Exit(result)
 	
@@ -32,56 +35,47 @@ func Menu() int {
 	
 	println("===========商品管理システム===========")
 	
-	err := Exists();
-	if err != nil {
-		log.Printf("debug: must make a file.")
-		err = CreateCSVFile();
-		log.Println(err)
-		if err != nil {
-			return 0
-		} else {
-			fmt.Printf("ファイルを作成しました。")
-			// ヘッダー実装
-			header := [][]string{
-				[]string{"ID","商品名", "原価", "売価", "定価", "在庫数", "商品コード"},
-			}
-			CsvFileWrite(header)
-		}	
-	} else {
-		log.Printf("debug: already make a file.")
-	}
+	
 	
 	AddProduct();
-	if err != nil {
-		log.Printf("alert: can`t write file")
-	}
 
-	// err = CsvFileRead();
-	// if err != nil {
-	// 	log.Printf("alert: can`t read file")
-	// }
+	records := ReadCsv(FILE_NAME)
+
+	DisplayRecords(records)
 
 	fmt.Printf("\n[追加: 1, 削除: 2, 更新: 3, 終了: 0]\n")
-
-	
 	
 	return 0
 }
 
+func FileInit() error {
+	csvExist := IsCsvExist();
+	if csvExist {
+		log.Printf("debug: already make a file.")			
+	} else {
+		log.Printf("debug: must make a file.")
 
+		err := CreateCSV();
+		if err != nil {
+			return err
+		} else {
+			fmt.Printf("ファイルを作成しました。")
+			// ヘッダー実装
+			header := []string{"No","商品名", "原価", "売価", "定価", "在庫数", "商品コード"}
+			WriteCsv(header)
+		}
+	}
+	return nil
+}
 
 // NOTE: ファイル存在確認関数
-func Exists() error {
+func IsCsvExist() bool {
 	_, err := os.Stat(FILE_NAME)
-	if !os.IsNotExist(err) {
-		// log.Fatal(err)
-		return nil
-	}
-	return err
+	return !os.IsNotExist(err)
 }
 
 // NOTE: ファイル作成関数
-func CreateCSVFile() error {
+func CreateCSV() error {
 	_, err := os.Create(FILE_NAME)
 	if err != nil {
 		log.Fatal(err)
@@ -91,106 +85,89 @@ func CreateCSVFile() error {
 }
 
 // NOTE: ファイル読み込み関数
-func CsvFileRead(list *[][]string) error {
-	// MEMO: Close不要
-	f, err := os.Open(FILE_NAME)		//ファイルの有無に関わらず書き込み権限で開くもの
+func ReadCsv(fileName string) [][]string {
+	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal(err)
-		return err
 	}
-	defer f.Close()
+	defer file.Close()
 
-	data := csv.NewReader(f)
+	r := csv.NewReader(file)
 
-	for {
-		row, err := data.Read() 		// csvを1行ずつ読み込む
-		  if err == io.EOF {
-			break 						// 読み込むべきデータが残っていない場合，Readはnil, io.EOFを返すため、ここでループを抜ける
-		  }
-		fmt.Println(row)
+	rows, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	return nil
+	return rows
 }
 
 // NOTE: ファイル書き込み関数
-func CsvFileWrite(record [][]string) error {
-	
-	// MEMO: Close不要
-	f, err := os.Create(FILE_NAME)		//ファイルの有無に関わらず書き込み権限で開くもの
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
-	
-	w := csv.NewWriter(f)
-	w.WriteAll(record)
+func WriteCsv(record []string) {
+	// レコード追加
+    file, err := os.OpenFile(FILE_NAME, os.O_WRONLY|os.O_APPEND, 0644)
+    if err != nil {
+        log.Fatal("Error:", err)
+    }
+    defer file.Close()
 
-	return nil
+    // 追加するレコードはstringのスライスで定義
+    writer := csv.NewWriter(file)
+    err = writer.Write(record)
+    if err != nil {
+        log.Fatal("Error:", err)
+    }
+    writer.Flush()
+}
+
+func DisplayRecords( records [][]string ) {
+	for _, record := range records {
+			fmt.Println(record)
+	}
+}
+
+func UpdateNo( records [][]string ) {
+	for no, record := range records {
+		record[0] = strconv.Itoa(no)
+	}
 }
 
 func AddProduct() error {
-	// var product_name string
-	// var cost_price int
-	// var selling_price int
-	// var list_price int
-	// var stock int
-	// var product_code string
+	var productName, productCode string
+	var costPrice, sellingPrice, listPrice, stock int
 
-	records := [][]string{
-		
-	}
+	csvData := ReadCsv(FILE_NAME)
 
-	CsvFileRead(&records)
+	//商品情報入力
+	fmt.Print("商品名：")
+	fmt.Scanf("%s", &productName)
 
-	data, err := ioutil.ReadFile(FILE_NAME)
-    if err != nil {
-        fmt.Println(err)
-    }
+	//原価値情報入力
+	fmt.Print("原価：")
+	fmt.Scanf("%d", &costPrice)
+	// if product.cost_price != int {
+	// 	return errors.New("数値ではありませんでした。")
+	// }
+	//売価値情報入力
+	fmt.Print("売価：")
+	fmt.Scanf("%d", &sellingPrice)
 
-	records = data
+	//定価値情報入力
+	fmt.Print("定価：")
+	fmt.Scanf("%d", &listPrice)
 
-    fmt.Println(string(data))
+	//在庫数情報入力
+	fmt.Print("在庫数：")
+	fmt.Scanf("%d", &stock)
+
+	//商品コード情報入力
+	fmt.Print("商品コード：")
+	fmt.Scanf("%s", &productCode)
 	
+	id := len(csvData)
+	add := []string{strconv.Itoa(id), productName, strconv.Itoa(costPrice), strconv.Itoa(sellingPrice), strconv.Itoa(listPrice),strconv.Itoa(stock), productCode}
 
-	// CsvFileRead(records);
-
-	// log.Println(records)
-
-	// //商品情報入力
-	// fmt.Print("商品名：")
-	// fmt.Scanf("%s", &product_name)
-
-	// //原価値情報入力
-	// fmt.Print("原価：")
-	// fmt.Scanf("%d", &cost_price)
-	// // if product.cost_price != int {
-	// // 	return errors.New("数値ではありませんでした。")
-	// // }
-	// //売価値情報入力
-	// fmt.Print("売価：")
-	// fmt.Scanf("%d", &selling_price)
-
-	// //定価値情報入力
-	// fmt.Print("定価：")
-	// fmt.Scanf("%d", &list_price)
-
-	// //在庫数情報入力
-	// fmt.Print("在庫数：")
-	// fmt.Scanf("%d", &stock)
-
-	// //商品コード情報入力
-	// fmt.Print("商品コード：")
-	// fmt.Scanf("%s", &product_code)
-	
-	id := len(rows)
-	// add := []string{strconv.Itoa(id), product_name, strconv.Itoa(cost_price), strconv.Itoa(selling_price), strconv.Itoa(list_price), strconv.Itoa(stock), product_code}
-	add := []string{strconv.Itoa(id), "abc", "100", "100", "100", "100", "100"}
-	fmt.Println(add)
-
-	// records = append(records, add)
-
-	// CsvFileWrite(records)
+	WriteCsv(add)
 
 	return nil
 
